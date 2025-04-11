1992068182f6c2e95be0733155a11fce25ab0433
Emerson <emersonnjunior2006@gmail.com>
2025-04-11 19:54:58 -0300

    add

--- obj/Debug/net9.0/Sistemadealuno.AssemblyInfo.cs
+++ obj/Debug/net9.0/Sistemadealuno.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sistemadealuno")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d07d2b48c7c9f238cdcea7cdb83e8c8d03384b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sistemadealuno")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sistemadealuno")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
